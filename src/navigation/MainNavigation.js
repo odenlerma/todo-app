582a6476452fff2f59a61e760ab975feef8067db
Audruey Gana <odenlerma@gmail.com>
2024-05-10 11:06:17 +0800

    - add listener to tasklist for mmkv storing

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -9,6 +9,9 @@ import AddTask from '@pages/AddTask';
 import ViewTask from '@pages/ViewTask';
 import EditTask from '@pages/EditTask';
 
+import { mmkv } from '@custom-redux/store';
+import { useSelector } from 'react-redux';
+
 const headlessScreenOptions = {
     headerMode: 'none',
     options: {
@@ -28,6 +31,12 @@ const MAIN_STACK_NAVIGATOR = () => (
 )
 
 export default () => {
+    const taskList = useSelector((state) => state.tasks);
+    useEffect(() => {
+        // LISTEN TO TASKS REDUCER CHANGES AND SAVE TO MMKV IF CHANGE OCCUR
+        mmkv.set('taskList', JSON.stringify(taskList));
+    }, [taskList])
+
     return(
         <NavigationContainer>
             <MAIN_STACK_NAVIGATOR />
