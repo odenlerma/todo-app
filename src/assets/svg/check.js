3f28f4b7fd0a5793d5d4103f0cc16f442d340cd8
Audruey Gana <odenlerma@gmail.com>
2024-05-10 14:21:52 +0800

    replace check and uncheck icon

--- src/assets/svg/check.js
+++ src/assets/svg/check.js
@@ -1,4 +1,5 @@
 export const CHECK_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="31" fill="none" viewBox="0 0 32 31">
-  <rect width="30" height="28" x="1" y="1.68" stroke="#4793AF" stroke-width="2" rx="4"/>
-  <path stroke="#4793AF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m24 9.68-11 11-5-5"/>
+<rect width="30" height="28" x="1" y="1.68" fill="#00993D" rx="14"/>
+<rect width="30" height="28" x="1" y="1.68" stroke="#00993D" stroke-width="2" rx="14"/>
+<path stroke="#FAFFFE" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m24 9.68-11 11-5-5"/>
 </svg>`
--- src/assets/svg/uncheck.js
+++ src/assets/svg/uncheck.js
@@ -1,3 +1,3 @@
 export const UNCHECKED_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 32 32">
-  <rect width="30" height="30" x="1" y="1" stroke="#4793AF" stroke-width="2" rx="4"/>
+<rect width="30" height="30" x="1" y="1" stroke="#849399" stroke-width="2" rx="15"/>
 </svg>`
